43a91a12e4a35506d823b1f20005e99061c80e04
Davinder Singh <davindersingh.asp@gmail.com>
2023-10-07 17:08:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_Sharp_Basics
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
