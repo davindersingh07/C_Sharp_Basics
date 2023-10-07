68880e7e12139300942a4f8edecea56611e0377d
Davinder Singh <davindersingh.asp@gmail.com>
2023-10-07 17:38:08 +0530

    Create a file, write to file and read from file.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace C_Sharp_Basics
 {
     public class Program
     {
         public static void Main(string[] args) 
-        { 
-            Console.WriteLine("Hello, World!");
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Example.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            //To write content to file
+            FileStream fs = File.Open(path, FileMode.Append);
+            byte[] info = new UTF8Encoding(true).GetBytes("Hello World!");
+            fs.Write(info, 0, info.Length);
+            fs.Close();
+
+            //To read content from file
+            StreamReader sr = new StreamReader(path);
+            string fileContent = sr.ReadToEnd();
+            Console.WriteLine(fileContent);
             Console.ReadLine();
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# C_Sharp_Basics
+Topic: 
+------
+CSharp Code to Create, Write, Read and Delete files on your Computer.
+
+What is File?
+-------------
+A File is Simple a Collection of Data. Files are Stored in non-volatile memory of computer.
+means they do not get erased/deleted when the power is turned off.
+
+Text File:
+---------
+Text file can be computer code, document like Microsoft Word, document composed of text.
+
+we store data in a file for several Reasons:
+1. The Data in the file requires long term storage.
+2. A file can be easily duplicated.
+3. Files are easily tansported.
+4. FIle storage is cheap. They do not take much space on computer.
+5. The File System is part of the operating system that organizes files and provides a way to create access, modify and delete files.
+
+ 
