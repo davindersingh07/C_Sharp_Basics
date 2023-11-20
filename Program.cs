90cb66ada77d3cdb197312c575911bf975aedff0
Davinder Singh <davindersingh.asp@gmail.com>
2023-11-20 23:37:01 +0530

    Code to print Star Pattern  given in ReadMe File.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Globalization;
 
 namespace C_Sharp_Basics
 {
     public class Program
     {
         public static void Main(string[] args) 
-        { 
-            Console.WriteLine("Hello, World!");
+        {
+
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Pattern_2(number);
             Console.ReadLine();
+        }
+        public static void Pattern_2(int n)
+        {
+            for(int i=1; i<=n; i++) 
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
