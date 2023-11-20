6c368279e22d511a4eeb04567afde8fca0030409
Davinder Singh <davindersingh.asp@gmail.com>
2023-11-20 23:27:14 +0530

    Code to print Start Pattern_1 given in ReadMe File.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,22 @@ namespace C_Sharp_Basics
     public class Program
     {
         public static void Main(string[] args) 
-        { 
-            Console.WriteLine("Hello, World!");
+        {
+            Console.WriteLine("Enter the Number");
+            int number =Convert.ToInt32(Console.ReadLine());
+            Pattern_1(number);
             Console.ReadLine();
+        }
+        public static void Pattern_1(int n) 
+        {
+            for(int i=1; i<=n; i++) 
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # C_Sharp_Basics
+
+Program to Print Star Pattern as:
+*
+* *
+* * *
+* * * * 
+* * * * * 
